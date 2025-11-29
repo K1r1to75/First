649ee105568b52c13554e16cd77d24a9cb318b6d
K1r1to75 <alexchita2008@mail.ru>
2025-11-29 11:09:25 +0900

    Add files via upload

--- C++/2nd/31.10.cpp
+++ C++/2nd/31.10.cpp
@@ -1,111 +1,77 @@
 ﻿#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
-
 int main() {
-    setlocale(LC_ALL, "ru");
-    srand(time(0));
+    srand(time(NULL));
+    int a;
+    a = 1;
+    cout << a << "\n\n";
 
-    // 1
-    const int ROWS = 3, COLS = 4;
-    int arr[ROWS][COLS] = {
-        {3, 5, 6, 7},
-        {12, 1, 1, 1},
-        {0, 7, 12, 1}
-    };
+    int num_1, num_1_1;
+    cout << "Введите число: ";
+    cin >> num_1;
+    num_1_1 = num_1 % 2;
+    if (num_1_1 == 0) {
+        cout << "Число четное!" << endl;
+    }
+    else {
+        cout << "Число нечетное!" << endl;
+    }
+    cout << "\n\n";
+    int i = 0;
+    for (; i <= 10;) {
+        cout << i * i * i << " ";
+        i++;
+    }
+    cout << "\n\n";
 
-    // Выводим исходный массив
-    cout << "Исходный массив:" << endl;
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            cout << arr[i][j] << " ";
+
+    int size;
+    cout << "Введите размер массива: ";
+    cin >> size;
+
+    int array[size];
+
+    cout << "Исходный массив:\n";
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand() % 100;
+        cout << array[i] << " ";
+    }
+    cout << "\n\n";
+
+    // Сортировка пузырьком
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
-        cout << endl;
     }
+    cout << "Отсортированный массив:\n";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+    cout << "\n\n";
 
-    // Вычисляем статистику
-    int sum = 0;
-    int min = arr[0][0];
-    int max = arr[0][0];
-
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            sum += arr[i][j];
-            if (arr[i][j] < min) min = arr[i][j];
-            if (arr[i][j] > max) max = arr[i][j];
+    const int m = 3;
+    const int n = 3;
+    int x = 1;
+    int mr[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            mr[i][j] = x;
+            x++;
+            cout << mr[i][j] << " ";
         }
+        cout << "\n\n";
     }
-
-    double average = (double)sum / (ROWS * COLS);
-
-    cout << "Сумма всех элементов: " << sum << endl;
-    cout << "Среднее арифметическое: " << average << endl;
-    cout << "Минимальный элемент: " << min << endl;
-    cout << "Максимальный элемент: " << max << endl;
-
-    // 2
-    int rowSum[ROWS] = { 0 };
-    int colSum[COLS] = { 0 };
-    int total = 0;
-
-    // Вычисляем суммы по строкам и столбцам
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            rowSum[i] += arr[i][j];
-            colSum[j] += arr[i][j];
-        }
-        total += rowSum[i];
-    }
-
-    // Выводим в требуемом формате
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            cout << arr[i][j] << "\t";
-        }
-        cout << "| " << rowSum[i] << endl;
-    }
-
-    // Разделительная линия
-    for (int i = 0; i < COLS; i++) {
-        cout << "---\t";
-    }
-    cout << "------" << endl;
-
-    // Суммы по столбцам и общая сумма
-    for (int j = 0; j < COLS; j++) {
-        cout << colSum[j] << "\t";
-    }
-    cout << "| " << total << endl;
-
-    // 3
-    const int ROWS1 = 5, COLS1 = 10;
-    const int ROWS2 = 5, COLS2 = 5;
-    int arr1[ROWS1][COLS1];
-    int arr2[ROWS2][COLS2];
-
-    // Заполняем первый массив случайными числами
-    cout << "Первый массив (5x10):" << endl;
-    for (int i = 0; i < ROWS1; i++) {
-        for (int j = 0; j < COLS1; j++) {
-            arr1[i][j] = rand() % 51; // 0-50
-            cout << arr1[i][j] << "\t";
-        }
-        cout << endl;
-    }
-
-    // Заполняем второй массив по правилу
-    cout << "\nВторой массив (5x5):" << endl;
-    for (int i = 0; i < ROWS2; i++) {
-        for (int j = 0; j < COLS2; j++) {
-            // Суммируем пары элементов из первого массива
-            int index1 = j * 2;      // первый элемент пары
-            int index2 = j * 2 + 1;  // второй элемент пары
-            arr2[i][j] = arr1[i][index1] + arr1[i][index2];
-            cout << arr2[i][j] << "\t";
-        }
-        cout << endl;
-    }
-
+    cout << "\n\n";
     return 0;
 }
