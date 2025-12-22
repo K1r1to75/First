cea788c1044387b20e8cb140de19b93fa179cb1e
K1r1to75 <alexchita2008@mail.ru>
2025-12-22 21:10:14 +0900

    Add files via upload

---
+++ C++/ChM/ChM_29.12.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f1(double x, double y, double z) {
+    return z;
+}
+
+double f2(double x, double y, double z) {
+    return -y;
+}
+
+// Метод Рунге-Кутта
+void runge_kutta(double x0, double y0, double z0, double h, int n) {
+    double x = x0, y = y0, z = z0;
+    for (int i = 0; i < n; i++) {
+        double k1 = f1(x, y, z);
+        double l1 = f2(x, y, z);
+
+        double k2 = f1(x + h / 2, y + h * k1 / 2, z + h * l1 / 2);
+        double l2 = f2(x + h / 2, y + h * k1 / 2, z + h * l1 / 2);
+
+        double k3 = f1(x + h / 2, y + h * k2 / 2, z + h * l2 / 2);
+        double l3 = f2(x + h / 2, y + h * k2 / 2, z + h * l2 / 2);
+
+        double k4 = f1(x + h, y + h * k3, z + h * l3);
+        double l4 = f2(x + h, y + h * k3, z + h * l3);
+
+        y += h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
+        z += h * (l1 + 2 * l2 + 2 * l3 + l4) / 6.0;
+        x += h;
+
+        cout << "x = " << x << ", y = " << y << ", z = " << z << endl;
+    }
+}
+
+int main() {
+    double x0, y0, z0, h;
+    int n;
+
+    cout << "Введите x0, y0, z0, h, n: ";
+    cin >> x0 >> y0 >> z0 >> h >> n;
+
+    runge_kutta(x0, y0, z0, h, n);
+
+    return 0;
+}
---
+++ C++/ChM/ChM_29.12.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x, double y) {
+    return x * x + y * y;
+}
+
+// Метод Эйлера
+void euler(double x0, double y0, double h, int n) {
+    double x = x0, y = y0;
+    for (int i = 0; i < n; i++) {
+        y += h * f(x, y);
+        x += h;
+        cout << "x = " << x << ", y = " << y << endl;
+    }
+}
+
+// Метод Эйлера-Коши
+void euler_c(double x0, double y0, double h, int n) {
+    double x = x0, y = y0;
+    for (int i = 0; i < n; i++) {
+        double k1 = f(x, y);
+        double y_t = y + h * k1;
+        double k2 = f(x + h, y_t);
+        y += h * (k1 + k2) / 2.0;
+        x += h;
+        cout << "x = " << x << ", y = " << y << endl;
+    }
+}
+
+// Метод Рунге-Кутта
+void runge_kutta(double x0, double y0, double h, int n) {
+    double x = x0, y = y0;
+    for (int i = 0; i < n; i++) {
+        double k1 = f(x, y);
+        double k2 = f(x + h / 2, y + h * k1 / 2);
+        double k3 = f(x + h / 2, y + h * k2 / 2);
+        double k4 = f(x + h, y + h * k3);
+        y += h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
+        x += h;
+        cout << "x = " << x << ", y = " << y << endl;
+    }
+}
+
+int main() {
+    int choice;
+    double x0, y0, h;
+    int n;
+
+    cout << "Введите x0, y0, h, n: ";
+    cin >> x0 >> y0 >> h >> n;
+
+    cout << "Выберите метод:\n1 - Эйлер\n2 - Эйлер-Коши\n3 - Рунге-Кутта 4\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1: euler(x0, y0, h, n); break;
+    case 2: euler_c(x0, y0, h, n); break;
+    case 3: runge_kutta(x0, y0, h, n); break;
+    default: cout << "Неверный выбор";
+    }
+
+    return 0;
+}
